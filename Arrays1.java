453c5a9903dbe2b77a6a73e8beeb23d716f0258d
reddyshabari <120706641+reddyshabari@users.noreply.github.com>
2023-02-21 10:53:47 +0530

    Arrays

--- Arrays1.java
+++ Arrays1.java
@@ -2,15 +2,15 @@ package Arrays;
 
 public class Arrays1 {
     public static void main(String[] args){
-    vector<int> sortedSquares(vector<int>& nums) {
+    vector<int> sortedSquares(vector<int>& n) {
       
       
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<n.size();i++)
         {
-            nums[i]=(nums[i]*nums[i]);
+            n[i]=(n[i]*n[i]);
         }
-          sort(nums.begin(),nums.end());
-        return nums;
+          sort(n.begin(),n.end());
+        return n;
         
     }
 }
